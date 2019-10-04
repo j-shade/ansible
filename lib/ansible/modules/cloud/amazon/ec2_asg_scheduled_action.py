1f2ea68c7917441acdbcf8ca6192720546126392
psharkey <psharkey@cleo.com>
2019-10-04 16:14:17 +0000

    Adding boilerplate code.

--- lib/ansible/modules/cloud/amazon/ec2_asg_scheduled_action.py
+++ lib/ansible/modules/cloud/amazon/ec2_asg_scheduled_action.py
@@ -21,6 +21,10 @@
 # and reporting changes. More flexibility on settings that can be set in the playbook, etc.
 #
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
